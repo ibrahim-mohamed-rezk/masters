a078c1756ba522e71ef5000ef68af30f0cc1f92b
ibrahim-mohamed-rezk <ibrahimmo989800@gmail.com>
2024-07-28 12:56:18 +0300

    tailwind production

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
         @vite('resources/css/app.css')
         <link rel="stylesheet" href="{{asset('css/styles-Ar/styles.css')}}">
         <link rel="stylesheet" href="{{asset('css/styles-Ar/header/index.css')}}">
+        <link rel="stylesheet" href="{{asset('build/assets/app-DvWVwaW3.css')}}">
         
         @yield('CssStyles')
 
@@ -20,7 +21,7 @@
         @include('layouts.footer')
         @yield('jsContainer')
 
-       
+       <script src="{{asset('build/assets/app-jtyrA5B8.js')}}"></script>
     </body>
 
 </html>
